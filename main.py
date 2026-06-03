8157fdf3c34eb0934deb98738510388f29ba8355
kalamkaskursabayeva <67894247+kalamkaskursabayeva@users.noreply.github.com>
2026-06-03 19:11:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -101,6 +101,12 @@ async def play_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton("➡️ Далее", callback_data="next_question")],
         [InlineKeyboardButton("🔙 Назад", callback_data="prev_question")],
+        [
+                    InlineKeyboardButton(
+                        "🔙 Назад на главную",
+                         callback_data="back_to_start",
+                    )
+                ],
     ]
     await update.callback_query.message.edit_text(
         text=text, reply_markup=InlineKeyboardMarkup(keyboard)
@@ -358,7 +364,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             [
                 [
                     InlineKeyboardButton(
-                        "🔗 Написать в WhatsApp", url="https://wa.me/7776314650?text=Хочу%20оформить%20заказ,%20промокод:%20Своим%20можно,%20я%20свой"
+                        "🔗 Написать в WhatsApp", url="https://wa.me/77776314650?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82!%20%F0%9F%91%8B%20%D0%A5%D0%BE%D1%87%D1%83%20%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D0%BA%D0%BD%D0%B8%D0%B3%D1%83%20%D1%81%20%D0%BF%D1%80%D0%BE%D0%BC%D0%BE%D0%BA%D0%BE%D0%B4%D0%BE%D0%BC%3A%D0%A1%D0%B2%D0%BE%D0%B8%D0%BC%20%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE%2C%20%D1%8F%20%D1%81%D0%B2%D0%BE%D0%B9%20"
                     )
                 ],
                 [
